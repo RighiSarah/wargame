039bdb4ce1276bab49391d4763ed1ce4894adeb0
kaldoran <thekingofthebigboss@hotmail.fr>
2013-11-09 18:19:21 +0100

    Change hero

--- local/bin/wargame/src/wargame/Heros.java
+++ local/bin/wargame/src/wargame/Heros.java
@@ -9,5 +9,11 @@ public class Heros extends Soldat {
 		this.puissance = type_heros.getPuissance();
 		this.tir = type_heros.getTir();
 	}
+	Heros(int heal, int range, int power, int shoot){
+		this.vie = heal;
+		this.portee = range;
+		this.puissance = power;
+		this.tir = shoot;
+	}
 
 }
